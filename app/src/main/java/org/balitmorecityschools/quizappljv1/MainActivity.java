363007c66f2955cffda83dbddbeb6d6953503257
Jones <1162570@bcpss.org>
2025-09-17 13:49:48 -0400

    First question, score screen

---
+++ app/src/main/java/org/balitmorecityschools/quizappljv1/MainActivity.java
@@ -0,0 +1,66 @@
+package org.balitmorecityschools.quizappljv1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    RadioButton dRB, selectedRB;
+    RadioGroup questionAnswersRG;
+    Button doneBTN;
+    int clickCounter, score;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        questionAnswersRG = findViewById(R.id.questionAnswers_RG);
+        doneBTN = findViewById(R.id.done_BTN);
+        dRB = findViewById(R.id.answerD_RB);
+        doneBTN.setVisibility(View.INVISIBLE);
+        clickCounter = 0;
+        score = 0;
+        questionAnswersRG.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull RadioGroup group, int checkedId) {
+                selectedRB = findViewById(checkedId);
+                doneBTN.setVisibility(View.VISIBLE);
+            }
+        });
+        doneBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickCounter = clickCounter + 1;
+                if (clickCounter == 1) {
+                    if (selectedRB.getId() == dRB.getId()) {
+                        score++;
+                        Toast correct = Toast.makeText(MainActivity.this, "Correct, Steve Kerr has the highest 3pt % in NBA history.", Toast.LENGTH_LONG);
+                        correct.show();
+                    }
+                    else {
+                        Toast incorrect = Toast.makeText(MainActivity.this, "Incorrect, Steve Kerr has the highest 3pt % in NBA history.", Toast.LENGTH_LONG);
+                        incorrect.show();
+                    }
+                    doneBTN.setText("CONTINUE");
+                }
+                if (clickCounter == 2) {
+                    Intent scoreScreen = new Intent(MainActivity.this, ScoreActivity.class);
+                    scoreScreen.putExtra("score", score);
+                    startActivity(scoreScreen);
+                }
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/org/balitmorecityschools/quizappljv1/ScoreActivity.java
@@ -0,0 +1,29 @@
+package org.balitmorecityschools.quizappljv1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ScoreActivity extends AppCompatActivity {
+    int score;
+    Intent incomingIntent;
+    TextView scoreTV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+        score = 0;
+        incomingIntent = getIntent();
+        score = incomingIntent.getIntExtra("score",score);
+        scoreTV = (TextView) findViewById(R.id.scoreTV);
+        scoreTV.setText("You've answered " + score + " question(s) correct so far!");
+
+    }
+}
